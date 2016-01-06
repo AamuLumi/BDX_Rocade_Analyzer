b45a4e5f175bb78de6d651bfc76ec9a9107c1b75
aamulumi <flokauder@hotmail.com>
2016-01-06 13:34:03 +0100

    feat(server): Add GET /since/:date route

--- server/server.js
+++ server/server.js
@@ -3,16 +3,24 @@
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
 let express = require('express'),
-    app = express();
+  app = express();
 
 let mongoose = require('mongoose'),
-    PartEntry = require('./PartEntry');
+  PartEntry = require('./PartEntry');
 
 // Mongoose Connection
 mongoose.connect('mongodb://localhost/roccade');
 mongoose.connection.on('error',
   console.error.bind(console, 'connection error:'));
 
+// Check if a date is valid (http://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript)
+function isValidDate(d) {
+  if (Object.prototype.toString.call(d) === "[object Date]")
+    return !isNaN(d.getTime());
+  else return false;
+}
+
+// Get all entries
 app.get('/all', (req, res) => {
   PartEntry.find().select('date trafficState partNumber').then((entries) => {
     res.status(200).send(entries);
@@ -20,6 +28,24 @@ app.get('/all', (req, res) => {
     res.status(500).send(err);
   });
 });
+
+// Get entries since a specific date
+app.get('/since/:date', (req, res) => {
+  let d = new Date(req.params.date);
+
+  if (!isValidDate(d))
+    res.status(400).send('Invalid Date');
+  else {
+    PartEntry.find({
+        date: {
+          $gte: d
+        }
+      }).select('date trafficState partNumber')
+      .then((entries) => {
+        res.status(200).send(entries);
+      })
+  }
+})
 
 app.listen(9900, () => {
   console.log("> Server is ready !");
