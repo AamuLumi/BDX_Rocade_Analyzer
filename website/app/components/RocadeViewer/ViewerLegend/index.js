8421653033d4b80112c63d539a996e21a840fdc2
aamulumi <flokauder@hotmail.com>
2016-05-03 16:46:26 +0200

    refactor(app): Change ViewerLegend to use constants in conf/Rocade

--- website/app/components/RocadeViewer/ViewerLegend/index.js
+++ website/app/components/RocadeViewer/ViewerLegend/index.js
@@ -1,35 +1,22 @@
 import React, {Component} from 'react';
+import Rocade from '~/conf/Rocade';
 import './ViewerLegend.less';
 
 export default class ViewerLegend extends Component {
   render() {
-    let legends = [
-      {
-        name: 'Fluide',
-        id: 'legendFluid'
-      }, {
-        name: 'Dense',
-        id: 'legendDense'
-      }, {
-        name: 'Saturé',
-        id: 'legendSaturated'
-      }, {
-        name: 'Bloqué',
-        id: 'legendBlocked'
-      }, {
-        name: 'Non disponible',
-        id: 'legendNotFound'
-      }
-    ];
     return (
       <div id="c-viewerLegend">
         <div className="title">Légende</div>
         <div className="legends">
-          {legends.map(function(e) {
+          {Rocade.trafficState.map(function(e) {
             return (
-              <div className="legend" key={e.name}>
-                <div id={e.id} className="legendCircle"></div>
-                {e.name}
+              <div className="legend" key={e.id}>
+                <div
+                  className="legendCircle"
+                  style={{
+                  'background-color': e.color
+                }}></div>
+              {e.description}
               </div>
             );
           })}
